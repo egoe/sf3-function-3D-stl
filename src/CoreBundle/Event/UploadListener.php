d498fdb75f6f3a101e5b9edb34e29910099d85b0
guillaume <guillaume@egoe.io>
2018-03-28 01:36:45 +0200

    Cleaning

--- src/CoreBundle/Event/UploadListener.php
+++ src/CoreBundle/Event/UploadListener.php
@@ -1,21 +1,16 @@
 <?php
 namespace CoreBundle\Event;
 
-use Doctrine\Common\Persistence\ObjectManager;
 use Oneup\UploaderBundle\Event\PostPersistEvent;
 
 class UploadListener
 {
-    /**
-     * @var ObjectManager
-     */
-    private $om;
 
-    //private $stlFileManager;
+    private $stlFileManager;
+    private $em;
 
     public function __construct( $em, $stlFileManager)
     {
-        //$this->om = $om;
         $this->stlFileManager = $stlFileManager;
         $this->em = $em;
     }
@@ -25,21 +20,16 @@ class UploadListener
         
         $file = $event->getFile();
 
-        //if everything went fine
         $response = $event->getResponse();
         $response['success'] = true;
         $response['file_name'] = $file->getFileName();
         $response['file_path'] = $file->getPathName();
 
         var_dump($this->stlFileManager->processStlFile());
-        //$response['file_extension'] = $file->getGuessExtension();
-        //$response['file_type_mime'] = $file->getMimeType();
-
-        //var_dump($file);
 
         return $response;
 
-        throw new UploadException('Nope, I don\'t do files.');
+        //throw new UploadException('Nope, I don\'t do files.');
 
     }
 }
--- src/CoreBundle/Manager/StlFileManager.php
+++ src/CoreBundle/Manager/StlFileManager.php
@@ -17,7 +17,6 @@ class StlFileManager
 
   /**
    * Processing STL FILE
-   *
    */
   public function processStlFile()
   {
